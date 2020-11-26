0d2d82b35fd6603697c3fb544d10fcd3ec2abbd6
NoaTzur <noatzur9@gmail.com>
2020-11-26 18:18:54 +0200

    new commit

--- power.c
+++ power.c
@@ -4,9 +4,16 @@
 double Exponent(int x) {
 	
 	double ans = 1;
-	
-	for(int i=0; i<x; i++){
-		ans = EXPO*ans;
+	if(x<0){
+		for(int i = x; i<0; i++) {
+			ans = 1.0/EXPO*ans;
+			}
+	}
+	else{
+
+		for(int i=0; i<x; i++){
+			ans = EXPO*ans;
+			}
 	}
 	
 	return ans;
@@ -15,9 +22,15 @@ double Exponent(int x) {
 double Power(double x, int y) {
 
 	double ans = 1;
-	
-	for(int i=0; i<y; i++){
-		ans = x*ans;
+	if(y<0){
+		for(int i=y; i<0; i++){
+			ans = 1.0/x*ans;
+		}
+	}
+	else{
+		for(int i=0; i<y; i++){
+			ans = x*ans;
+		}
 	}
 	
 	return ans;
